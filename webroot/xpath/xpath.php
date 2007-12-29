9676aaac8b68319ae266ae5767304409247714c7
Andres Riancho <andres.riancho@gmail.com>
2007-12-29 13:03:32 +0000

    Added the motw plugin created by Sharad Ganapathy.

---
+++ webroot/xpath/xpath.php
@@ -0,0 +1,13 @@
+<?
+
+  if ( strcmp($_GET['i'], "d'z'0") == 0)
+  {
+    # Sorry, I had no time to setup a ldap server
+     echo "org.apache.xpath.XPath";
+  }
+  else
+  {
+    echo $_GET['i'];
+  }
+                
+?>

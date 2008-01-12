85797301aca46df2f0f95cec239605a96b1a617f
Andres Riancho <andres.riancho@gmail.com>
2008-01-12 00:14:45 +0000

    Fixed some bugs in audit.xsrf that showed duplicated URLs and some errors with cookies in the same plugin.

--- webroot/xsrf/xsrf.php
+++ webroot/xsrf/xsrf.php
@@ -1,7 +1,5 @@
 <?php
 $value = 'something from somewhere';
 
-setcookie("TestCookie", $value);
-setcookie("TestCookie", $value, time()+3600);  /* expire in 1 hour */
-setcookie("TestCookie", $value, time()+3600, "/~rasmus/", ".example.com", 1);
+setcookie("TestCookie", $value, time()+60*60*24*30, "/~rasmus/", ".example.com", 1);
 ?> 

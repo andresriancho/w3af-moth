9af75e0fd746ab1f5a9b419d71bbdd3caf3d9cdb
Andres Riancho <andres.riancho@gmail.com>
2008-01-14 01:51:10 +0000

    Fixed some bugs that were discovered with "./w3af -t" .

--- webroot/xpath/xpath.php
+++ webroot/xpath/xpath.php
@@ -1,6 +1,6 @@
 <?
 
-  if ( strcmp($_GET['i'], "d'z'0") == 0)
+  if ( strcmp($_GET['i'], "d'z\"0") == 0)
   {
     # Sorry, I had no time to setup a ldap server
      echo "org.apache.xpath.XPath";

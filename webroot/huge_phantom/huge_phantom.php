93f32c68baa031a75085a95384abcb452f16829f
Andres Riancho <andres.riancho@gmail.com>
2008-07-11 03:03:06 +0000

    Some test scripts and other changes to debug memory consumption issues.

---
+++ webroot/huge_phantom/huge_phantom.php
@@ -0,0 +1,30 @@
+<?
+
+# This script creates A LOT of links that point to random urls
+# that are really this same script.
+
+function random_letters ($numofletters) {
+    $res = '';
+    $literki = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W');
+    $ilosc_literek = count($literki);
+    for ($licz = 0; $licz < $numofletters; $licz++) {
+	    $rand = rand(0, $ilosc_literek-1);
+	    $vercode = $vercode.$literki[$rand];
+	    $res .= $vercode;
+    }
+    return $res;
+}
+
+echo 'First, some links:';
+echo '<a href="' . random_letters( 6 ) . '">' . random_letters( 6 ) . '</a><br/>';
+echo '<a href="' . random_letters( 6 ) . '">' . random_letters( 6 ) . '</a><br/>';
+echo '<a href="' . random_letters( 6 ) . '">' . random_letters( 6 ) . '</a><br/>';
+echo '<br/><br/><br/>';
+
+echo 'And now some random stuff to use up some memory:<br/>';
+
+for ($i = 0; $i < 400; $i++) {
+    echo random_letters( 20 ) . '<br/>';
+}
+
+?>

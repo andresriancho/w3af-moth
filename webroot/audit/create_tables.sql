0118ad54fc547f1c73225e445e0e3ff84366efb3
Andres Riancho <andres.riancho@gmail.com>
2008-09-28 16:52:48 +0000

    New table structure for the SQL injection tests.

--- webroot/audit/create_tables.sql
+++ webroot/audit/create_tables.sql
@@ -3,21 +3,17 @@
 #    1) mysql -u root -p -h localhost
 #    2) mysql> source create_tables.sql
 #
+drop database w3af_test;
 
 create database w3af_test;
 
 use w3af_test;
 
-create table agenda(
-  id int not null auto_increment primary key,
-  nombre VARCHAR(100),
-  direccion VARCHAR(100),
-  telefono VARCHAR(100),
-  email VARCHAR(100)
-);
+# This is on one line, because of recreate_tables.php
+create table users( id int not null auto_increment primary key,  name VARCHAR(100),  address VARCHAR(100),  phone VARCHAR(100),  email VARCHAR(100));
 
-insert into agenda (nombre,direccion,telefono,email) values ('andres','chac 1981','47789900','a@b.com');
-insert into agenda (nombre,direccion,telefono,email) values ('pablo','case 2382','54110099','caa@acm.org');
-insert into agenda (nombre,direccion,telefono,email) values ('juan','foos 2331','7985776','isnot@db.org');
-insert into agenda (nombre,direccion,telefono,email) values ('carlos','bar 31','122333','acho@cybsec.com');
+insert into users (name,address,phone,email) values ('andres','chac 1981','47789900','a@b.com');
+insert into users (name,address,phone,email) values ('pablo','case 2382','54110099','caa@acm.org');
+insert into users (name,address,phone,email) values ('juan','foos 2331','7985776','isnot@db.org');
+insert into users (name,address,phone,email) values ('carlos','bar 31','122333','acho@cybsec.com');
 

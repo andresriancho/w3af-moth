58fc42876929d69187e6fdc6bad5f6da8adae42d
Andres Riancho <andres.riancho@gmail.com>
2008-10-19 01:20:07 +0000

    More updates to the phishing tests.

--- webroot/phishingVector/phishingVector.php
+++
@@ -1,8 +0,0 @@
-<?
-
-echo '<iframe id="datamain0" name="info" marginwidth="1" marginheight="1" border="0" frameborder="0" width="555" height="402" src="' . $_GET['url'] .'"  hspace="0" vspace="0" frameborder="0"> ';
-echo 'El Navegador que esta utilizando no soporta	el contenido de esta p&aacute;gina<br>';
-echo 'le recomendamos que actualice su navegador a Internet Explorer 5.5+ ó Netscape 6.0+.';
-echo '</iframe>'                          
-
-?>

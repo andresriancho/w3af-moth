fca05e13b4822177d20bb40f0d7b380a62d38e0c
Andres Riancho <andres.riancho@gmail.com>
2008-10-19 01:33:08 +0000

    Added more test scripts for the phishing vector plugin. Also:
    - added more tests to the plugin
    - the tests I added "evade" the most stupid filters
    - fixed some pylint warnings

---
+++ webroot/audit/phishing_vector/frame_phishing.php
@@ -0,0 +1,6 @@
+<?
+
+echo '<frame id="datamain0" name="info" marginwidth="1" marginheight="1" border="0" frameborder="0" width="555" height="402" src="' . $_GET['url'] .'"  hspace="0" vspace="0" frameborder="0"> ';
+echo '</frame>'                          
+
+?>
---
+++ webroot/audit/phishing_vector/http_blacklist_phishing.php
@@ -0,0 +1,6 @@
+<?
+
+echo '<iframe id="datamain0" name="info" marginwidth="1" marginheight="1" border="0" frameborder="0" width="555" height="402" src="' . str_ireplace('http://','',$_GET['section']) .'"  hspace="0" vspace="0" frameborder="0"> ';
+echo '</iframe>'                          
+
+?>

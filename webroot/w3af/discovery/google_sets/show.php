5883545a79d71591c1460ac60bcf4d051d7055d1
Andres Riancho <andres.riancho@gmail.com>
2008-12-25 13:31:51 +0000

    Test files for google sets.

---
+++ webroot/w3af/discovery/google_sets/show.php
@@ -0,0 +1,23 @@
+<?
+
+if ( strcmp( $_GET['os'],"linux") == 0 ){
+
+echo 'we like linux';
+
+}
+
+
+if ( strcmp( $_GET['os'],"unix") == 0 ){
+
+echo 'we love unix';
+
+}
+
+if ( strcmp( $_GET['os'],"windows") == 0 ){
+
+echo 'ohh... you found out that we hate windows';
+
+}
+
+
+?>

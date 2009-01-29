dc794c0862d02e943859caebf7ae2bcb487ff906
Andres Riancho <andres.riancho@gmail.com>
2009-01-29 11:44:00 +0000

    Updating the WML parser testing scripts.

---
+++ webroot/w3af/core/wml_parser/sql_injection_string.php
@@ -0,0 +1,24 @@
+<?
+
+$link = mysql_connect("localhost", "root", "chauchas!");
+
+mysql_select_db("w3af_test", $link);
+
+$sql_stm = "SELECT * FROM users where name='" . $_GET["name"] . "'";
+#echo $sql_stm;
+
+$result = mysql_query( $sql_stm, $link);
+
+if (!$result) {
+  $exceptionstring = "Error performing query: $sql_stm: <br />";
+  $exceptionstring .= mysql_errno() . ": " . mysql_error();
+  throw new Exception($exceptionstring);
+} else {
+                                                            
+  echo "<b>Name:</b> ".mysql_result($result, 0, "name")."<br>";    
+  echo "<b>Address:</b>  ".mysql_result($result, 0, "address")."<br>";           
+  echo "<b>Phone:</b> ".mysql_result($result, 0, "phone")."<br>";      
+  echo "<b>Email:</b> ".mysql_result($result, 0, "email")."<br>"; 
+}
+
+?>

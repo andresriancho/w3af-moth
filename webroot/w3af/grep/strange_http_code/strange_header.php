f69fec24907b7a2d232ba042db6da59d1986be90
Andres Riancho <andres.riancho@gmail.com>
2009-02-02 12:27:32 +0000

    Test scripts for strange HTTP codes.

--- webroot/w3af/grep/strange_http_code/strange_header.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('w3af-rocks!: http://www.example.com/');
-?>
-Start --
-
-I just sent you a strange header.
-
--- End
-
---
+++ webroot/w3af/grep/strange_http_code/strange_http_code.php
@@ -0,0 +1,9 @@
+<?php
+header("HTTP/1.0 527 Not Found");
+?>
+Start --
+
+I just sent you a strange HTTP Code.
+
+-- End
+

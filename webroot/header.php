99311a576c5213a8d17bf351ca060a47185ebd22
Andres Riancho <andres.riancho@gmail.com>
2009-03-02 12:33:09 +0000

    Better file structure.

---
+++ webroot/header.php
@@ -0,0 +1,24 @@
+<!-- ###### Header ###### -->
+<div id="top">
+    <div id="header">
+				<div id="headerTitle"><img src="/images/w3af.png" alt="w3af logo" align="left" border="0"></div>
+				<br />
+				<!-- ###### Navigation Bar ###### -->
+				<div id="navBar">          
+          <div id="navBarTitle">Table of Contents</div>
+
+					<a href="http://w3af.sf.net/">w3af homepage</a>
+					<a href="http://sourceforge.net/tracker/?group_id=170274&atid=853652">Report a bug</a>
+                    <br />
+					<a href="#w3af">w3af tests</a>
+					<a href="#wivet">wivet</a>
+					<a href="#SiteGenerator">SiteGenerator</a>
+					<a href="#vulnerable-web-applications">Web Applications</a>
+					<a href="#misc">Other files</a>
+                    <br />
+
+					<div class="navBarTitle" align="center">
+                    <a href="http://www.bonsai-sec.com/"><img src="/images/bonsai_tiny_75x75.png" alt="Bonsai - Information Security"></a>
+    </div>
+</div>
+<!-- ###### End Header ###### -->

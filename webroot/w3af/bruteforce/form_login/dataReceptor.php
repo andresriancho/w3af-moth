542d139bde58fc4929f77de77639a586b394d90b
Javier Andalia <javier.andalia@rapid7.com>
2011-09-05 22:58:42 +0000

    Added new login page accessible by GET

--- webroot/w3af/bruteforce/form_login/dataReceptor.php
+++ webroot/w3af/bruteforce/form_login/dataReceptor.php
@@ -1,30 +1,46 @@
 <?
 
+$user = $_REQUEST['user'];
+$pwd = $_REQUEST['pass'];
+
+if ($user){
+
+    if (strcmp($pwd, "") == 0){
+        echo "fill the blanks <br>";
+    }
+
+    else if (strcmp($user, "admin") == 0 && strcmp($pwd, "1234") == 0){ 
+        setcookie("magicValue", "0000-1111-2222-3333-4444", time()+3600);
+        setcookie("magicValue2", "0000-1111-2222-3333-4444", time()+3600);
+        
+        echo "Login OK! <br>";
+        echo $user;
+	echo "<table>
+		<tr>
+			<th>
+			Welcome to our admin panel. Next you may see our menu option where you'll be able to manage the whole website. Isn't it cool???
+			</th>
+		</tr>
+		<tr>
+			<td>
+			<div>OPTION-1|OPTION-2|OPTION-3|OPTION-4|OPTION-5|OPTION-6|OPTION-7</div>
+			</td>
+		</tr>
+	      </table>";
+
+    }
+    else{
+        echo "Bad login, stop bruteforcing me!<br>";
+        echo "Bad u/p combination for user: " .  $user;
+    }
+}
+
+echo "<br><br>";
 if ( strcmp( $_COOKIE["magicValue"] , '0000-1111-2222-3333-4444') == 0 ){
   echo '<a href="onlyUsers.php">onlyUsers.php</a>';
 }
-  
 
-if ( strcmp($_POST['pass'],"") == 0 ){
-        echo "fill the blanks <br>";
-        die();
-}   
-                                    
-
-if ( strcmp($_POST['user'],"admin") == 0 && strcmp($_POST['pass'],"1234") == 0 ){ 
-	setcookie("magicValue", "0000-1111-2222-3333-4444", time()+3600);
-	setcookie("magicValue2", "0000-1111-2222-3333-4444", time()+3600);
-	
-	echo "Login OK! <br>";
-	echo $_POST['user'];
-
-}
-else
-{
-	echo "Bad login, stop bruteforcing me!";
-	echo "Bad u/p combination for user: " .  $_POST['user'];
-
-}
+echo "<br>";
 
 ?>
 <br/>

8c33ef5bd0ac608925adade58594bb528af3e739
Andres Riancho <andres.riancho@gmail.com>
2012-07-27 13:15:09 +0000

    Adding test scripts for the cookie_handler.py file in core/data/url/handlers/

---
+++ webroot/moth/w3af/core/cookie_handler/has-cookie.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_COOKIE['TestCookie'])) {
+  echo 'Cookie was sent.';
+}else{
+  echo 'Cookie was NOT sent.';
+}
+
+?> 
---
+++ webroot/moth/w3af/core/cookie_handler/set-cookie.php
@@ -0,0 +1,7 @@
+<?php
+
+$value = 'something from somewhere';
+setcookie("TestCookie", $value);
+
+?> 
+Your browser has a cookie now.

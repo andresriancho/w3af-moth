26a3039948454b6fbcabd45e13ff135b03a0281d
Andres Riancho <andres.riancho@gmail.com>
2012-09-19 21:35:54 +0000

    No insults :D

--- webroot/moth/w3af/grep/error_500/500.php
+++ webroot/moth/w3af/grep/error_500/500.php
@@ -3,7 +3,7 @@
 if ( strcmp($_GET['id'],'1')==0){
   echo 'Ok! HTTP response code is 200.';
 }else{
-  header("HTTP/1.0 500 Fuck off!");
+  header("HTTP/1.0 500 Error!");
   echo 'Error! HTTP response code is 500.';
 }
 

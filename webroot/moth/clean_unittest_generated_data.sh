c119ddd8cdc5980dfe5594e2bce800d8cc787da8
Andres Riancho <andres.riancho@gmail.com>
2012-10-18 13:46:36 +0000

    Make sure the files are world writable

--- webroot/moth/clean_unittest_generated_data.sh
+++ webroot/moth/clean_unittest_generated_data.sh
@@ -4,3 +4,5 @@ sudo rm -rf w3af/audit/file_upload/uploads/*
 sudo rm -rf w3af/audit/dav/write-all/*
 sudo svn revert w3af/audit/ssi/messages.shtml
 sudo svn revert w3af/audit/xss/stored/data.txt
+sudo chmod 777 w3af/audit/ssi/messages.shtml
+sudo chmod 777 w3af/audit/xss/stored/data.txt

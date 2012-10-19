83b827f3e93905879539532429959f7dc8271058
Andres Riancho <andres.riancho@gmail.com>
2012-10-19 19:17:01 +0000

    Adding tests for handling of spaces (' ' vs %20 vs +)

---
+++ webroot/moth/w3af/core/encoding/spaces/queryxpath.php
@@ -0,0 +1,10 @@
+<?
+
+if ($_REQUEST['__VIEWSTATE'] === '/wEPDwUKMTEzMDczNTAxOWRk' && $_REQUEST['__EVENTVALIDATION'] === '/wEWAwLNx+2YBwKw59eKCgKcjoPABw=='){
+  echo $_REQUEST['_ctl0:_ctl0:Content:Main:TextBox1'];
+}
+else
+{
+  echo 'Viewstate and/or eventvalidation error!';
+}
+?>

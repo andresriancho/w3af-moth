5c60f13d1d40998867d79f89bd2966ae5f4d94b4
Andres Riancho <andres.riancho@gmail.com>
2012-12-11 15:34:18 +0000

    $_POST -> $_REQUEST , just to make things easier.

--- webroot/moth/w3af/audit/xpath/xpath-tag.php
+++ webroot/moth/w3af/audit/xpath/xpath-tag.php
@@ -3,8 +3,8 @@ include("funcs.php");
 
 echo 'HEADER>>> ', generateRandStr(50), '<br><br>';
 
-if (array_key_exists('input', $_POST)) {
-	$input = $_POST["input"];
+if (array_key_exists('input', $_REQUEST)) {
+	$input = $_REQUEST["input"];
 	printQueryResult("/articles/article/" . $input);
 }
 

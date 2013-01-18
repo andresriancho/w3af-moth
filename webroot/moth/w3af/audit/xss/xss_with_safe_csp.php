541435e4c690cf2af86e353876c9fd8edfb46a81
Andres Riancho <andres.riancho@gmail.com>
2013-01-18 13:19:00 +0000

    Adding test files for CSP

---
+++ webroot/moth/w3af/audit/xss/xss_with_safe_csp.php
@@ -0,0 +1,7 @@
+<?
+
+header("Content-Security-Policy: default-src 'self'");
+
+echo $_GET['data'];
+
+?>
---
+++ webroot/moth/w3af/audit/xss/xss_with_weak_csp.php
@@ -0,0 +1,7 @@
+<?
+
+header("X-Content-Security-Policy: script-src *;");
+
+echo $_GET['data'];
+
+?>

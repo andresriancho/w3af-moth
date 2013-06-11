a8e34938c027fd5385918182dae5e9565406dfe7
Andres Riancho <andres.riancho@gmail.com>
2013-06-11 16:13:04 -0300

    Last (?) of our migrations away from SVN

--- webroot/moth/clean_unittest_generated_data.sh
+++ webroot/moth/clean_unittest_generated_data.sh
@@ -2,7 +2,7 @@
 
 sudo rm -rf w3af/audit/file_upload/uploads/*
 sudo rm -rf w3af/audit/dav/write-all/*
-sudo svn revert w3af/audit/ssi/messages.shtml
-sudo svn revert w3af/audit/xss/stored/data.txt
+sudo git checkout w3af/audit/ssi/messages.shtml
+sudo git checkout w3af/audit/xss/stored/data.txt
 sudo chmod 777 w3af/audit/ssi/messages.shtml
 sudo chmod 777 w3af/audit/xss/stored/data.txt
